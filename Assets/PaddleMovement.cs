07299cf26c37d6feb4a33d01cc4bfef9b89d2d0a
martinsomer <martinsom35@gmail.com>
2019-04-12 00:28:28 +0300

    Added paddle

---
+++ Assets/PaddleMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleMovement : MonoBehaviour {
+    private Vector3 mousePosition;
+    private float moveSpeed = 1f;
+    
+    // Update is called once per frame
+    void Update() {
+        mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        mousePosition.y = transform.position.y;
+        transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+    }
+}
