41117687a78e1f4fbb2091522bf38bbbcea49026
martinsomer <martinsom35@gmail.com>
2019-04-12 02:45:19 +0300

    Added paddle collisions with walls

--- Assets/PaddleMovement.cs
+++ Assets/PaddleMovement.cs
@@ -6,11 +6,33 @@ public class PaddleMovement : MonoBehaviour {
     private Vector3 mousePosition;
     private float moveSpeed = 1f;
     
-    // Update is called once per frame
-    void Update() {
+    public GameObject leftWall;
+    public GameObject rightWall;
+    
+    void Awake() {
+        leftWall = GameObject.Find("LeftWall");
+        rightWall = GameObject.Find("RightWall");
+    }
+    
+    // Update is called for rendering and handling GUI events
+    void OnGUI() {
         mousePosition = Input.mousePosition;
         mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
         mousePosition.y = transform.position.y;
+        
+        // Touching left wall
+        if ((transform.position.x - transform.localScale.x/2) < (leftWall.GetComponent<Transform>().position.x + leftWall.GetComponent<Transform>().localScale.x/2)) {
+            transform.position = Vector2.Lerp(transform.position, new Vector2(((leftWall.GetComponent<Transform>().position.x + leftWall.GetComponent<Transform>().localScale.x/2) + transform.localScale.x/2), transform.position.y), moveSpeed);
+            return;
+        }
+        
+        // Touching right wall
+        if (transform.position.x + transform.localScale.x/2 > rightWall.GetComponent<Transform>().position.x - rightWall.GetComponent<Transform>().localScale.x/2) {
+            transform.position = Vector2.Lerp(transform.position, new Vector2(((rightWall.GetComponent<Transform>().position.x - rightWall.GetComponent<Transform>().localScale.x/2) - transform.localScale.x/2), transform.position.y), moveSpeed);
+            return;
+        }
+        
+        // Not touching wall
         transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
     }
 }
