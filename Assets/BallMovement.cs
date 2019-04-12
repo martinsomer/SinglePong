4f92ef9da5ec5291a47401474294dc46e856dba6
martinsomer <martinsom35@gmail.com>
2019-04-12 23:21:02 +0300

    Added ball and physics

---
+++ Assets/BallMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour {
+    //public bool gameStarted = false;
+    private float velocity;
+    private Rigidbody2D ball;
+    public GameObject player;
+    
+    void Awake() {
+        player = GameObject.Find("Player");
+        ball = GetComponent<Rigidbody2D>();
+    }
+    
+    void Start() {
+        float pos_x = player.GetComponent<Transform>().position.x;
+        float pos_y = player.GetComponent<Transform>().position.y + player.GetComponent<Transform>().localScale.y/2 + transform.localScale.y/2;
+        
+        transform.position = Vector2.Lerp(transform.position, new Vector2(pos_x, pos_y), 1);
+        
+        float limit_x;
+        do {
+            limit_x = Random.Range(-1.0f, 1.0f);
+        } while (limit_x > -0.3f && limit_x < 0.3f);
+        float limit_y = 1;
+        
+        Vector2 randomVector = new Vector2(limit_x, limit_y);
+        ball.AddRelativeForce(randomVector * 100);
+    }
+    
+    void OnCollisionEnter2D(Collision2D collision) {
+        
+        if (collision.gameObject.name != "Player") {
+            return;
+        }
+        
+        if (ball.velocity.x > 25.0f || ball.velocity.y > 25.0f) {
+            return;
+        }
+            
+        ball.AddForce(ball.velocity.normalized * 1.1f, ForceMode2D.Impulse);
+        Debug.Log(ball.velocity);
+    }
+}
--- Assets/PaddleMovement.cs
+++ Assets/PaddleMovement.cs
@@ -14,21 +14,28 @@ public class PaddleMovement : MonoBehaviour {
         rightWall = GameObject.Find("RightWall");
     }
     
-    // Update is called for rendering and handling GUI events
     void OnGUI() {
         mousePosition = Input.mousePosition;
         mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
         mousePosition.y = transform.position.y;
         
         // Touching left wall
-        if ((transform.position.x - transform.localScale.x/2) < (leftWall.GetComponent<Transform>().position.x + leftWall.GetComponent<Transform>().localScale.x/2)) {
-            transform.position = Vector2.Lerp(transform.position, new Vector2(((leftWall.GetComponent<Transform>().position.x + leftWall.GetComponent<Transform>().localScale.x/2) + transform.localScale.x/2), transform.position.y), moveSpeed);
+        if (transform.position.x - transform.localScale.x/2 < leftWall.GetComponent<Transform>().position.x + leftWall.GetComponent<Transform>().localScale.x/2) {
+            
+            float pos_x = leftWall.GetComponent<Transform>().position.x + leftWall.GetComponent<Transform>().localScale.x/2 + transform.localScale.x/2;
+            float pos_y = transform.position.y;
+            
+            transform.position = Vector2.Lerp(transform.position, new Vector2(pos_x, pos_y), moveSpeed);
             return;
         }
         
         // Touching right wall
         if (transform.position.x + transform.localScale.x/2 > rightWall.GetComponent<Transform>().position.x - rightWall.GetComponent<Transform>().localScale.x/2) {
-            transform.position = Vector2.Lerp(transform.position, new Vector2(((rightWall.GetComponent<Transform>().position.x - rightWall.GetComponent<Transform>().localScale.x/2) - transform.localScale.x/2), transform.position.y), moveSpeed);
+
+            float pos_x = rightWall.GetComponent<Transform>().position.x - rightWall.GetComponent<Transform>().localScale.x/2 - transform.localScale.x/2;
+            float pos_y = transform.position.y;
+            
+            transform.position = Vector2.Lerp(transform.position, new Vector2(pos_x, pos_y), moveSpeed);
             return;
         }
         
